e4d92b7e5c450fbbb2393cc567de7ad7e0a625ba
Oliver Oliverio <oliveroliverio@gmail.com>
2021-12-02 18:33:34 -0800

    bunch of practice

--- Section03/01-starting-setup/src/components/ExpenseItem.js
+++ Section03/01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
-import './ExpenseItem.css';
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>March 28, 2021</div>
+      <div></div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
 }
-// be sure to export
+
 export default ExpenseItem;
