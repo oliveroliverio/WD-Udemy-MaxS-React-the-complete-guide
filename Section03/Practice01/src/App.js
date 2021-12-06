b748cf481ae48db91fed8efdba4691135ce49fc0
Oliver Oliverio <oliveroliverio@gmail.com>
2021-12-05 17:21:24 -0800

    20211205_17:21

--- Section03/Practice01/src/App.js
+++ Section03/Practice01/src/App.js
@@ -1,9 +1,20 @@
+import Test from "./Test";
+
+
 function App() {
+
+  let variable = 'qwer';
+
+
+
   return (
     <div>
       <h2>Let's get started!</h2>
-
-
+      <Test foo={variable}> </Test>
+      <Test foo={variable}> </Test>
+      <Test foo={variable}> </Test>
+      <Test foo={variable}> </Test>
+      <Test foo={variable}> </Test>
     </div>
   );
 }
---
+++ Section03/Practice01/src/Test.js
@@ -0,0 +1,7 @@
+function Test(props){
+  return (
+    <h1>{props.foo}</h1>
+  )
+}
+
+export default Test
